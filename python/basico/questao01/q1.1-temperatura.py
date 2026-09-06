980278fc864c3f94683f4a7c21780a917327da03
samuellins <rnsamueldlins@gmail.com>
2026-09-06 20:48:56 -0300

    Create q1.1-temperatura.py

---
+++ python/basico/questao01/q1.1-temperatura.py
@@ -0,0 +1,22 @@
+'''
+Conversor de Temperatura
+
+'''
+
+temp_em_c = float(input('Digite uma temperatura (C°): '))
+
+temp_em_f = (temp_em_c * 9/5) + 32
+
+print(f'{temp_em_c}°C equivale a {temp_em_f}°F.')
+
+if temp_em_c <= 0:
+    print(f'Classificação: Congelando')
+
+elif temp_em_c >= 1 and temp_em_c <= 15:
+    print(f'Classificação: Frio')
+
+elif  temp_em_c >= 16 and temp_em_c <= 25:
+    print(f'Classificação: Agradável')
+
+else:
+    print(f'Classificação: Quente')
