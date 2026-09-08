6fff0398c7d6c9d7a763fb1ad5805999f9f05f7c
samuellins <rnsamueldlins@gmail.com>
2026-09-08 16:04:57 -0300

    Create q1-var.ts

---
+++ typescripts/bloco 01 - variaveis/questao01/q1-var.ts
@@ -0,0 +1,6 @@
+let nome: string = 'Samuel'
+let idade: number = 18
+let estudandoTS: boolean = true
+let listaLinguagens: string[] = ['python', 'javascript', 'typescript']
+
+console.log(nome, idade, estudandoTS, listaLinguagens)
