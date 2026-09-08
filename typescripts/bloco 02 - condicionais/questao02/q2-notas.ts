b5f36c9259c36bdd4d8a4bc59d8a31c59445df5c
samuellins <rnsamueldlins@gmail.com>
2026-09-08 16:05:02 -0300

    Create q2-notas.ts

---
+++ typescripts/bloco 02 - condicionais/questao02/q2-notas.ts
@@ -0,0 +1,14 @@
+let nota: number = 9
+
+if (nota >= 9) {
+    console.log('Excelente')
+    
+} else if (nota >= 7) {
+    console.log('Bom')
+
+} else if (nota >= 5) {
+    console.log('Regular')
+
+} else {
+    console.log('Reprovado')
+}
