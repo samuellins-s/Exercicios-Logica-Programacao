783decdca2f885066884832ba3e421c39bf5fbc6
samuellins <rnsamueldlins@gmail.com>
2026-09-08 18:21:31 -0300

    Create q3-while.ts

---
+++ typescripts/bloco 03 - lacos/questao03/q3-while.ts
@@ -0,0 +1,9 @@
+let numero: number = 7
+let resultadoMultiplicacao: number = 0
+let i: number = 1
+
+while (i <= 10) {
+    resultadoMultiplicacao = numero * i
+    console.log(`${numero} x ${i} = ${resultadoMultiplicacao}`)
+    i++
+}
